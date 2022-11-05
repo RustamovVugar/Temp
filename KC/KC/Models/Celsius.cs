3a8455fbfa20272d7cb405210bedeb0cf135c1cd
CeferovaSona <87385383+CeferovaSona@users.noreply.github.com>
2022-11-05 20:30:21 +0400

    create Temper folder

---
+++ KC/KC/Models/Celsius.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KC.Models
+{
+    public class Celsius
+    {
+        public double Degree { get; set; }
+
+        public Celsius(double degree)
+        {
+            Degree = degree;
+        }
+
+        public static implicit operator Kelvin(Celsius celsius)
+        {
+            Kelvin k1 = new Kelvin((celsius.Degree * 9 / 5) + 32);
+            return k1;
+        }
+    }
+}
---
+++ KC/KC/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KC
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius c1 = new Celsius(0);
+            Kelvin k1 = c1;
+            Console.WriteLine(k1.Degree);
+        }
+    }
+}
